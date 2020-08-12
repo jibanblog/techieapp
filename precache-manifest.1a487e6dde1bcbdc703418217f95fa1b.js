de77aed7c7cf0853061ecf60b910029f801b0dc8
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-08-12 10:47:14 +0530

    Updates

---
+++ precache-manifest.1a487e6dde1bcbdc703418217f95fa1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5c3aa1eb0a9260d1032c0ed4719d71",
+    "url": "/techieapp/index.html"
+  },
+  {
+    "revision": "1f88c5e1d169e67866ef",
+    "url": "/techieapp/static/css/main.1c264a47.chunk.css"
+  },
+  {
+    "revision": "a020009621d1d11f5449",
+    "url": "/techieapp/static/js/2.94ff7f93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/techieapp/static/js/2.94ff7f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f88c5e1d169e67866ef",
+    "url": "/techieapp/static/js/main.51b7c563.chunk.js"
+  },
+  {
+    "revision": "b97d4b92c863b367594c",
+    "url": "/techieapp/static/js/runtime-main.6828241b.js"
+  }
+]);
